48ae6a7c4484bed9eda0c58d9a75c21cb4612ab2
Paul McSlarrow <pmcslarrow@icloud.com>
2024-02-13 11:59:42 -0800

    Initial Commit

---
+++ Hoop Easy/Hoop_EasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hoop_EasyApp.swift
+//  Hoop Easy
+//
+//  Created by Paul McSlarrow on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Hoop_EasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
